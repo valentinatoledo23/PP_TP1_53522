456a098da37aa78662827c9740e46a65300f0778
Valentina Toledo <valentina.toledo.utn@gmail.com>
2026-09-02 15:28:19 -0300

    TP1 Paradigmas

--- src/App.java
+++ src/App.java
@@ -7,42 +7,29 @@ public class App {
 
         //--------------------------INSTANCIAS--------------------------
 
-        //Creación de salas
+        //Creación de sala
         Sala sala1 = new Sala( //Agregacion: la sala se crea afuera de EventoUniversitario
                 1,
                 "Aula Multimedia"
         );
 
-        Sala sala2 = new Sala(
-                2,
-                "Aula Básica"
-        );
-
-        //Creación de eventos
+        //Creación de evento
         EventoUniversitario evento1 = new EventoUniversitario(
                 "EV01",
-                "Charla informativa sobre Phython",
-                25000,
-                false
-        );
-        EventoUniversitario evento2 = new EventoUniversitario(
-                "EV02",
                 "Charla informativa sobre Java",
-                21000,
+                25000,
                 false
         );
 
         //Creación de actividades
         evento1.crearActividad(11, "Problemáticas actuales en el mundo digital", 35, "Charla", "Carlos Pereyra");
-        evento2.crearActividad(23, "Programación Orientada a Objetos", 40, "Taller", true);
+        evento1.crearActividad(23, "Programación Orientada a Objetos", 40, "Taller", true);
 
-        //Asignacion de salas
+        //Asignacion de sala
         evento1.asignarSala(sala1); //Agregacion: la sala existe independientemente de EventoUniversitario
-        evento2.asignarSala(sala2);
 
-        //Copias de los eventos y actividades
+        //Copias del evento y actividades
         EventoUniversitario copiaEvento1 = new EventoUniversitario(evento1);
-        EventoUniversitario copiaEvento2 = new EventoUniversitario(evento2);
 
         //Creacion de estudiantes
         Estudiante estudiante1= new Estudiante(
@@ -55,23 +42,27 @@ public class App {
                 "Albana Mattos"
         );
 
+        Estudiante estudiante3= new Estudiante(
+                "53772",
+                "Jimena Perez"
+        );
+
         //--------------------------LISTAS--------------------------
 
         List<Estudiante> estudiantes = new ArrayList<>();
         estudiantes.add(estudiante1);
         estudiantes.add(estudiante2);
+        estudiantes.add(estudiante3);
 
-        evento1.getActividades().get(0).inscribir(estudiante1);
-        evento2.getActividades().get(0).inscribir(estudiante2);
+        evento1.getActividades().get(0).inscribir(estudiante1); //get(0) representa a Charla porque se creo primero
+        evento1.getActividades().get(0).inscribir(estudiante2);
+        evento1.getActividades().get(1).inscribir(estudiante1); //get(1) representa a Taller porque se creo segundo
+        evento1.getActividades().get(1).inscribir(estudiante3);
 
         //--------------------------MUESTRAS DE DATOS--------------------------
         evento1.mostrarDatos();
         copiaEvento1.mostrarDatos();
 
-        evento2.mostrarDatos();
-        copiaEvento2.mostrarDatos();
-
         System.out.println("Total de eventos creados: " + EventoUniversitario.getCantidadEventos());
-
     }
 }
